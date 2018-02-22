4d7dfb45ce50d1f73de717612c1048d0d44bb913
KLY <993005983@qq.com>
2018-02-22 16:02:54 +0800

    1

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -183,24 +183,6 @@ class CountryController extends Controller
             return $this->render('add', ['model' => $model]);
         }
     }
-     public function actionIndex()
-    {
-        $query = Country::find();
-
-        $pagination = new Pagination([
-            'defaultPageSize' => 5,
-            'totalCount' => $query->count(),
-        ]);
-
-        $countries = $query->orderBy('name')
-            ->offset($pagination->offset)
-            ->limit($pagination->limit)
-            ->all();
-
-        return $this->render('index', [
-            'countries' => $countries,
-            'pagination' => $pagination,
-        ]);
-    }
+   
 }
 ?>
